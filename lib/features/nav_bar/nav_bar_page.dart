ff3bdf29e4ed3880146fb215a117df6c605b6da5
OLADISEA <ladisea55@gmail.com>
2024-06-12 22:01:45 +0100

    Added the NavBarPage with Persistent Bottom Navigation Bar

---
+++ lib/features/nav_bar/nav_bar_page.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
+import 'package:trivia/features/home/presentation/pages/home_page.dart';
+
+class NavBarPage extends StatelessWidget {
+  NavBarPage({Key? key}) : super(key: key);
+
+  //late bool _hideNavBar;
+  final PersistentTabController _controller = PersistentTabController(initialIndex: 0);
+
+  // @override
+  List<Widget> _buildScreens() {
+    return [
+      const HomePage(),
+      //Center(child: Text('Home Screen')),
+      Center(child: Text('Leaders Board')),
+      Center(child: Text('Profile Screen')),
+    ];
+  }
+
+  List<PersistentBottomNavBarItem> _navBarsItems() {
+    return [
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.home),
+        title: ("Home"),
+        activeColorPrimary: Colors.blue,
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.search),
+        title: ("Search"),
+        activeColorPrimary: Colors.blue,
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.person),
+        title: ("Profile"),
+        activeColorPrimary: Colors.blue,
+        inactiveColorPrimary: Colors.grey,
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PersistentTabView(
+        context,
+        controller: _controller,
+        screens: _buildScreens(),
+        items: _navBarsItems(),
+        confineInSafeArea: true,
+        backgroundColor: Colors.white, // Default is Colors.white.
+        handleAndroidBackButtonPress: true, // Default is true.
+        resizeToAvoidBottomInset: true, // This needs to be true if you want to move up the screen when keyboard appears.
+        stateManagement: true, // Default is true.
+        hideNavigationBarWhenKeyboardShows: true, // Recommended to set 'true' to hide bottom nav bar when keyboard appears.
+        decoration: NavBarDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          colorBehindNavBar: Colors.white,
+        ),
+        popAllScreensOnTapOfSelectedTab: true,
+        popActionScreens: PopActionScreensType.all,
+        itemAnimationProperties: ItemAnimationProperties(
+          duration: Duration(milliseconds: 200),
+          curve: Curves.ease,
+        ),
+        screenTransitionAnimation: ScreenTransitionAnimation(
+          animateTabTransition: true,
+          curve: Curves.ease,
+          duration: Duration(milliseconds: 200),
+        ),
+        navBarStyle: NavBarStyle.style6, // Choose the nav bar style with this property.
+      ),
+    );
+  }
+}
