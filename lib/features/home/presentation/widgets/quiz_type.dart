83449bd22dac5ce01bbb3dd6004dd82bb120c064
OLADISEA <ladisea55@gmail.com>
2024-06-13 23:51:24 +0100

    Created the quiz type widget which show the type of quiz the user can select

---
+++ lib/features/home/presentation/widgets/quiz_type.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:trivia/features/auth/presentation/widgets/submit_button.dart';
+
+class QuizType extends StatelessWidget {
+  const QuizType({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SubmitButton(text: "Random Quiz", color: Colors.blueAccent,textColor: Colors.white,width: 150.w,),
+        SizedBox(width: 10.w,),
+        SubmitButton(text: "Custom Quiz", color: Colors.blueAccent,textColor: Colors.white,width: 150.w,)
+
+      ],
+    );
+  }
+}
