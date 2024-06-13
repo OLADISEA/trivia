ef260f94e687bacd8206a11142f605c1c0085071
OLADISEA <ladisea55@gmail.com>
2024-06-13 23:48:01 +0100

    Created the app text button

---
+++ lib/features/auth/presentation/widgets/text_button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:trivia/core/common/widgets/reusable_text.dart';
+
+class AppTextButton extends StatelessWidget {
+  final String text;
+  final Color color;
+  final double fontSize;
+  final void Function()? onTap;
+  const AppTextButton({super.key, required this.text, this.onTap, required this.color, required this.fontSize});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        onTap: onTap,
+        child: Reusable(text: text,textColor: color,fontSize: fontSize,)
+    );
+  }
+}
