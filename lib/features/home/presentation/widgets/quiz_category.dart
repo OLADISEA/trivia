6a91b4334c56e56cad2f10dcabce60106132bb35
OLADISEA <ladisea55@gmail.com>
2024-06-17 22:28:20 +0100

    Included the category data in the grid view builder

--- lib/features/home/presentation/widgets/quiz_category.dart
+++ lib/features/home/presentation/widgets/quiz_category.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:trivia/core/common/widgets/reusable_text.dart';
+import 'package:trivia/features/quiz/data/category_data.dart';
 import 'package:trivia/features/quiz/presentation/quiz_page.dart';
+import 'package:trivia/features/quiz/presentation/quiz_parameter_page.dart';
 
 class QuizCategory extends StatelessWidget {
   const QuizCategory({super.key});
@@ -10,7 +12,7 @@ class QuizCategory extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       child: GridView.builder(
-          itemCount: 4,
+          itemCount: categories.length,
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2, // Number of items per row
             crossAxisSpacing: 10.w, // Horizontal spacing between items
@@ -20,15 +22,19 @@ class QuizCategory extends StatelessWidget {
           itemBuilder: (context, index){
             final imageList = ["general_knowledge.jpg","books.jpg","music.png","computer.jpg"];
             final titleList = ["General knowledge","Books","Music","Computer"];
-            return gridItem(context: context,image: imageList[index], quizTitle: titleList[index], questionsNum: "1400");
+            return gridItem(context: context,image: categories[index+1].asset, quizTitle: categories[index+1].name, questionsNum: "1400",);
           }),
     );
   }
 
- Widget gridItem({required BuildContext context,required String image, required String quizTitle, required String questionsNum}){
+ Widget gridItem({
+   required BuildContext context,
+   required String image,
+   required String quizTitle,
+   required String questionsNum}){
     return InkWell(
         onTap: (){
-          Navigator.of(context).push(MaterialPageRoute(builder: (context)=> QuizPage()));
+          Navigator.of(context).push(MaterialPageRoute(builder: (context)=> QuizParameterPage(title: quizTitle,)));
         },
         child: Container(
           height: 300.h,
@@ -40,7 +46,13 @@ class QuizCategory extends StatelessWidget {
                 color: Colors.black.withOpacity(0.5),
                 spreadRadius: 1,
                 blurRadius: 1,
-                offset: Offset(-2,3)
+                offset: Offset(-1,2)
+              ),
+              BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 1,
+                  blurRadius: 1,
+                  offset: Offset(0.5,-1)
               )
             ]
           ),
@@ -52,18 +64,18 @@ class QuizCategory extends StatelessWidget {
               Positioned(
                   top: -12.h,
                   left: 15.w,
-                  child: Image.asset("assets/images/$image",width: 100.w,height: 80.h,)
+                  child: Image.asset("assets/category/$image",width: 100.w,height: 80.h,)
               ),
               //SizedBox(height: 70.h,),
               Container(
-                margin: EdgeInsets.only(top: 75.h),
+                margin: EdgeInsets.only(top: 80.h),
                   //height: 50.h,
                   //width: 100.w,
-                  child: Reusable(text: quizTitle,fontSize: 18.sp,textAlign: TextAlign.left,)),
+                  child: Reusable(text: quizTitle,fontSize: 20.sp,textAlign: TextAlign.left,fontWeight: FontWeight.w700,)),
               //SizedBox(height: 5.h),
               Container(
-                  margin: EdgeInsets.only(top: 100.h),
-                  child: Reusable(text: '$questionsNum questions'))
+                  margin: EdgeInsets.only(top: 110.h),
+                  child: Reusable(text: '$questionsNum questions',fontSize: 15.sp,))
             ],
           ),
         ),
