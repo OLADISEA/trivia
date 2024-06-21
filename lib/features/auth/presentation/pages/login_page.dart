9860dd5c13ddce933fe36e7ae19f7ad9b376ffb2
OLADISEA <ladisea55@gmail.com>
2024-06-21 12:17:25 +0100

    Added the circular progress indicator when trying to sign in.

--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -19,63 +19,73 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: EdgeInsets.only(left: 30.w,top: 50.h,right: 30.w),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-                child: Image.asset("assets/images/quiz-welcome.jpg",width: 200.w,height: 200.h,
-                ),
-            ),
-            SizedBox(height: 50.h,),
-            Reusable(text: "Login",fontSize: 25.sp,),
-            SizedBox(height: 15.h,),
-            AuthTextField(controller: emailController, text: "Email"),
-            SizedBox(height: 10.h,),
-            AuthTextField(controller: passwordController, text: "Password",obscureText: true,),
-            SizedBox(height: 10.h,),
-            Container(
-              margin: EdgeInsets.only(left: 240.w),
-              child: AppTextButton(text: "Forgot Password?",color: Colors.blueAccent,fontSize: 12.sp,
-              onTap: (){
-                Navigator.of(context).push(MaterialPageRoute(builder: (context)=> ForgotPasswordPage()));
-
-              },),
-            ),
-            SizedBox(height: 20.h,),
-            BlocConsumer<AuthBloc, AuthState>(
-              builder: (BuildContext context, state) {
-                return SubmitButton(text: "LOGIN", color: Colors.lightBlue,textColor: Colors.white,
-                    onTap: (){
-                      context.read<AuthBloc>().add(LoginRequested(emailController.text, passwordController.text));
-                    });
-              },
-              listener: (BuildContext context, Object? state) {
-                if(state is AuthAuthenticated){
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context)=> NavBarPage()));
-                }
-              },
-            ),
-            SizedBox(height: 100.h,),
-
-            Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Reusable(text: "New Here?",fontSize: 15.sp,),
-                  AppTextButton(text: "Register", color: Colors.blueAccent,fontSize: 15.sp,onTap: (){
-                    print(true);
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context)=> RegisterPage()));
-
-                  },)
-                ],
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.only(left: 30.w,top: 50.h,right: 30.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                  child: Image.asset("assets/images/quiz-welcome.jpg",width: 200.w,height: 200.h,
+                  ),
               ),
-            )
-
-
-
-          ],
+              SizedBox(height: 50.h,),
+              Reusable(text: "Login",fontSize: 25.sp,),
+              SizedBox(height: 15.h,),
+              AuthTextField(controller: emailController, text: "Email"),
+              SizedBox(height: 10.h,),
+              AuthTextField(controller: passwordController, text: "Password",obscureText: true,),
+              SizedBox(height: 10.h,),
+              Container(
+                margin: EdgeInsets.only(left: 240.w),
+                child: AppTextButton(text: "Forgot Password?",color: Colors.blueAccent,fontSize: 12.sp,
+                onTap: (){
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context)=> ForgotPasswordPage()));
+        
+                },),
+              ),
+              SizedBox(height: 20.h,),
+              BlocConsumer<AuthBloc, AuthState>(
+                builder: (BuildContext context, state) {
+                  if(state is AuthLoading){
+                    return const Center(
+                        child: CircularProgressIndicator());
+                  }
+                  return SubmitButton(text: "LOGIN", color: Colors.lightBlue,textColor: Colors.white,
+                      onTap: (){
+                        context.read<AuthBloc>().add(LoginRequested(emailController.text, passwordController.text));
+                      });
+                },
+                listener: (BuildContext context, Object? state) {
+                  if(state is AuthAuthenticated){
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context)=> NavBarPage()));
+                  }
+                  if(state is AuthError){
+                    final error = state.message;
+                    print(error);
+                  }
+                },
+              ),
+              SizedBox(height: 100.h,),
+        
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Reusable(text: "New Here?",fontSize: 15.sp,),
+                    AppTextButton(text: "Register", color: Colors.blueAccent,fontSize: 15.sp,onTap: (){
+                      print(true);
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> RegisterPage()));
+        
+                    },)
+                  ],
+                ),
+              )
+        
+        
+        
+            ],
+          ),
         ),
       ),
     );
