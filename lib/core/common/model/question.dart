8e63e63fb1d9c3af42fde9af8189e8d26a5856db
OLADISEA <ladisea55@gmail.com>
2024-06-21 12:26:37 +0100

    Made an adjustment in the Question model

--- lib/core/common/model/question.dart
+++ lib/core/common/model/question.dart
@@ -10,9 +10,14 @@ class Question {
   });
 
   factory Question.fromJson(Map<String, dynamic> json) {
+    // Combine correct_answer and incorrect_answers into a single list
+    List<String> answers = List<String>.from(json['incorrect_answers']);
+    answers.add(json['correct_answer']);
+    answers.shuffle();  // Shuffle to randomize the order
+
     return Question(
       question: json['question'],
-      answers: List<String>.from(json['answers']),
+      answers: answers,
       correctAnswer: json['correct_answer'],
     );
   }
