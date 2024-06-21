6cd897a951bba582a13e5ef56e0abd88608e2dc9
OLADISEA <ladisea55@gmail.com>
2024-06-21 12:30:27 +0100

    Implemented the update user score to update the user score whenever the user has a new high score

--- lib/features/quiz/bloc/quiz_bloc.dart
+++ lib/features/quiz/bloc/quiz_bloc.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:trivia/features/quiz/data/quiz_repository.dart';
 import 'package:trivia/features/quiz/bloc/quiz_event.dart';
@@ -6,19 +7,25 @@ import 'package:trivia/features/quiz/bloc/quiz_state.dart';
 
 class QuizBloc extends Bloc<QuizEvent, QuizState> {
   final QuizRepository quizRepository;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
   Timer? _timer;
   int _userScore = 0;
-  List<String?> _userAnswers = []; // Store user-selected answers for each question
 
-  QuizBloc({required this.quizRepository}) : super(QuizState(questions: [], currentQuestionIndex: 0, remainingTime: 30,userAnswers: [])) {
+  QuizBloc({required this.quizRepository}) : super(const QuizState()) {
     on<LoadQuestions>(_onLoadQuestions);
     on<SelectAnswer>(_onSelectAnswer);
     on<NextQuestion>(_onNextQuestion);
     on<UpdateTimer>(_onUpdateTimer);
+    on<UpdateUserScore>(_updateUserScore);
   }
 
   Future<void> _onLoadQuestions(LoadQuestions event, Emitter<QuizState> emit) async {
     try {
+      // Reset score and timer when loading new questions
+      resetScore();
+      _timer?.cancel();
+
       final questions = await quizRepository.fetchQuestions(
         amount: event.amount,
         category: event.category,
@@ -27,14 +34,12 @@ class QuizBloc extends Bloc<QuizEvent, QuizState> {
         encoding: event.encoding,
       );
 
-
       // Initialize userAnswers with the correct length and null values
       final newUserAnswers = List<String?>.filled(questions.length, null);
 
-
       print("The bloc state has the set of questions");
       print(questions.length);
-      final newState = state.copyWith(questions: questions,userAnswers: newUserAnswers);
+      final newState = state.copyWith(questions: questions, userAnswers: newUserAnswers);
       emit(newState);
 
       // Print after emitting the new state
@@ -47,13 +52,11 @@ class QuizBloc extends Bloc<QuizEvent, QuizState> {
 
   void _onSelectAnswer(SelectAnswer event, Emitter<QuizState> emit) {
     final isCorrect = event.answer == state.questions[state.currentQuestionIndex].correctAnswer;
-    final updatedScore = isCorrect? _userScore + 1: _userScore;
-
+    final updatedScore = isCorrect ? _userScore + 1 : _userScore;
 
     // Update userAnswers for the current question
     List<String?> newUserAnswers = List.from(state.userAnswers);
     newUserAnswers[state.currentQuestionIndex] = event.answer;
-
 
     emit(state.copyWith(
       selectedAnswer: event.answer,
@@ -90,9 +93,41 @@ class QuizBloc extends Bloc<QuizEvent, QuizState> {
         } else {
           timer.cancel();
           // Handle quiz end
+          add(UpdateUserScore(userScore: _userScore, userId: 'user_id')); // Add userId as required
         }
       }
     });
+  }
+
+  void _updateUserScore(UpdateUserScore event, Emitter<QuizState> emit) async {
+    //emit(QuizLoadingState());
+    print(event.userId);
+    print('this is the user id ${event.userId}');
+    DocumentReference userRef = _firestore.collection('users').doc(event.userId);
+
+    await _firestore.runTransaction((transaction) async {
+      DocumentSnapshot snapshot = await transaction.get(userRef);
+
+      if (!snapshot.exists) {
+        throw Exception("User does not exist!");
+      }
+
+      int currentHighScore = snapshot['highScore'];
+
+      if (event.userScore > currentHighScore) {
+        transaction.update(userRef, {'highScore': event.userScore});
+      }
+    });
+    print('the state.question length is ${state.questions}');
+
+    emit(QuizSubmittedState(
+      questions: state.questions,
+      currentQuestionIndex: state.currentQuestionIndex,
+      remainingTime: state.remainingTime,
+      selectedAnswer: state.selectedAnswer,
+      correctAnswer: state.correctAnswer,
+      userAnswers: state.userAnswers,
+    ));
   }
 
   int get correctAnswersCount => _userScore;
@@ -106,7 +141,7 @@ class QuizBloc extends Bloc<QuizEvent, QuizState> {
   Future<void> close() {
     _timer?.cancel();
     // Reset the state to initial values
-    emit(QuizState(questions: [], currentQuestionIndex: 0, remainingTime: 30,userAnswers: []));
+    emit(QuizState(questions: [], currentQuestionIndex: 0, remainingTime: 30, userAnswers: []));
     return super.close();
   }
 }
