829afbc9ae5f87ffea2d1a22fea73c728b8d5bbf
OLADISEA <ladisea55@gmail.com>
2024-06-22 10:52:46 +0100

    Add Forgot Password page UI layout and implementation.

--- lib/features/auth/presentation/pages/forgot_password.dart
+++ lib/features/auth/presentation/pages/forgot_password.dart
@@ -15,32 +15,34 @@ class ForgotPasswordPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        margin: EdgeInsets.only(left: 30.w,top: 50.h,right: 30.w),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Image.asset("assets/images/quiz-welcome.jpg",width: 200.w,height: 200.h,
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.only(left: 30.w,top: 50.h,right: 30.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Image.asset("assets/images/quiz-welcome.jpg",width: 200.w,height: 200.h,
+                ),
               ),
-            ),
-            SizedBox(height: 50.h,),
-            Container(
-              height: 70.h,
-                width: 125.w,
-                child: Reusable(text: "Forgot Password?",fontSize: 25.sp,textAlign: TextAlign.left,)),
-            SizedBox(height: 15.h,),
-            Reusable(text: "Please enter the email address associated with your account",fontSize: 15.sp,textAlign: TextAlign.left),
-            SizedBox(height: 10.h,),
-            AuthTextField(controller: passwordController, text: "Password",obscureText: true,),
-            SizedBox(height: 150.h,),
-            const SubmitButton(text: "SUBMIT", color: Colors.lightBlue),
-            SizedBox(height: 100.h,),
-
-
-
-
-          ],
+              SizedBox(height: 50.h,),
+              SizedBox(
+                height: 70.h,
+                  width: 125.w,
+                  child: Reusable(text: "Forgot Password?",fontSize: 25.sp,textAlign: TextAlign.left,fontWeight: FontWeight.w500,)),
+              SizedBox(height: 15.h,),
+              Reusable(text: "Please enter the email address associated with your account",fontSize: 15.sp,textAlign: TextAlign.left),
+              SizedBox(height: 10.h,),
+              AuthTextField(controller: passwordController, text: "Password",obscureText: true,),
+              SizedBox(height: 150.h,),
+              const SubmitButton(text: "SUBMIT", color: Colors.lightBlue,textColor: Colors.white,),
+              SizedBox(height: 100.h,),
+        
+        
+        
+        
+            ],
+          ),
         ),
       ),
     );
