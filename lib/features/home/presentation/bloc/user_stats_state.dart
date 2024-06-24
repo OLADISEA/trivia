57ffaf5420a8d44eedc9543e8db2caebe8c11700
OLADISEA <ladisea55@gmail.com>
2024-06-24 16:42:38 +0100

    Create the user stats state

---
+++ lib/features/home/presentation/bloc/user_stats_state.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+import 'package:trivia/core/common/model/user_statistics.dart';
+
+abstract class UserStatsState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class UserStatsInitial extends UserStatsState {}
+
+class UserStatsLoading extends UserStatsState {}
+
+class UserStatsLoaded extends UserStatsState {
+  final UserStatistics stats;
+
+  UserStatsLoaded(this.stats);
+
+  @override
+  List<Object?> get props => [stats];
+}
+
+class UserStatsSaved extends UserStatsState {}
+
+class UserStatsError extends UserStatsState {
+  final String message;
+
+  UserStatsError(this.message);
+
+  @override
+  List<Object?> get props => [message];
+}
