20e7adf20f6fd088ca9c3db4bf4c6c18c52a53ca
OLADISEA <ladisea55@gmail.com>
2025-01-11 02:20:25 +0100

    feat: add app colours

--- lib/config/app_colors.dart
+++ lib/config/app_colors.dart
@@ -3,5 +3,7 @@ import 'dart:ui';
 class AppColor{
 
   static const Color textColor = Color(0xffffffff);
+  static const Color gradientColor1 = const Color(0xFF2E98E9);
+  static const Color gradientColor2 = const Color(0xFF7F39FB);
 
 }
